329cc41f7f010ebfdef699e9181cbeb8531c4761
Thibaut <79365056+jthibaut@users.noreply.github.com>
2025-08-27 16:43:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from fastapi import FastAPI, File, UploadFile
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from io import BytesIO
+
+from fastapi import FastAPI
+from app.routes import cleaning
 
 app = FastAPI()
 
-@app.post("/clean")
-async def clean_data(file: UploadFile = File(...)):
-    contents = await file.read()
-    df = pd.read_csv(BytesIO(contents))
-
-    # Nettoyage
-    df.drop_duplicates(inplace=True)
-    df.fillna(df.mean(numeric_only=True), inplace=True)
-
-    # Normalisation
-    num_cols = df.select_dtypes(include='number').columns
-    scaler = StandardScaler()
-    df[num_cols] = scaler.fit_transform(df[num_cols])
-
-    # Retourner le fichier nettoyé
-    return df.to_json(orient="records")
+app.include_router(cleaning.router)
